4cb587dc1d6b660fe5354b0173e3524f1d442477
Phillip Abbott <phillip@phillips-m1-max.home>
2023-04-11 23:14:40 +0100

    Hell Mary - JDTLS working - kinda

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -1,19 +1,20 @@
-local home = os.getenv("HOME")
-local jdtls = require("jdtls")
-
-print("home -> " .. home)
-
--- File types that signify a Java project's root directory. This will be
--- used by eclipse to determine what constitutes a workspace
-local root_markers = { "gradlew", "mvnw", ".git", "pom.xml", "gradle.build" }
+local jdtls_dir = vim.fn.stdpath("data") .. "/mason/packages/jdtls"
+local config_dir = jdtls_dir .. "/config_mac"
+local plugins_dir = jdtls_dir .. "/plugins"
+local path_to_jar = plugins_dir .. "/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar"
+local path_to_lombok = jdtls_dir .. "/lombok.jar"
+local root_markers = { "gradlew", "mvnw", ".git", "pom.xml", "build.gradle" }
 local root_dir = require("jdtls.setup").find_root(root_markers)
 
--- eclipse.jdt.ls stores project specific data within a folder. If you are working
--- with multiple different projects, each project must use a dedicated data directory.
--- This variable is used to configure eclipse to use the directory name of the
--- current project found using the root_marker as the folder for project specific data.
-local workspace_folder = home .. "/.cache/jdtls/workspace/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
-print("workspace set to -->" .. workspace_folder)
+local home = "/Users/phillip"
+if root_dir == "" then
+  return
+end
+
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), "p:h:t")
+local workspace_dir = vim.fn.stdpath("data") .. "/site/java/workspace-root" .. project_name
+os.execute("mkdir " .. workspace_dir)
+
 -- Helper function for creating keymaps
 function nnoremap(rhs, lhs, bufopts, desc)
   bufopts.desc = desc
@@ -63,28 +64,82 @@ end
 print("** keymappings assigned **")
 
 local config = {
-  flags = {
-    debounce_text_changes = 80,
+  cmd = {
+    home .. "/.sdkman/candidates/java/17.0.6-tem/bin/java",
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    "-Dlog.protocol=true",
+    "-Dlog.level=ALL",
+    "-javaagent:" .. path_to_lombok,
+    "-Xms1g",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+    -- If you use lombok, download the lombok jar and place it in ~/.local/share/eclipse
+    --"-javaagent:" .. home .. "/.config/nvim/java/lombok.jar",
+
+    "-jar",
+    path_to_jar,
+
+    "-configuration",
+    config_dir,
+
+    "-data",
+    workspace_dir,
   },
-  on_attach = on_attach, -- We pass our on_attach keybindings to the configuration map
+
   root_dir = root_dir, -- Set the root directory to our found root_marker
-  -- Here you can configure eclipse.jdt.ls specific settings
-  -- These are defined by the eclipse.jdt.ls project and will be passed to eclipse when starting.
-  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-  -- for a list of options
+
   settings = {
     java = {
-      format = {
-        settings = {
-          -- Use Google Java style guidelines for formatting
-          -- To use, make sure to download the file from https://github.com/google/styleguide/blob/gh-pages/eclipse-java-google-style.xml
-          -- and place it in the ~/.local/share/eclipse directory
-          url = home .. "/.config/nvim/java/checkstyle.xml",
-          profile = "modpmstyle",
+      home = home .. "/.sdkman/candidates/java/17.0.6-tem",
+      eclipse = {
+        downloadSources = true,
+      },
+      configuration = {
+        updateBuildConfiguration = "interactive",
+        runtimes = {
+          {
+            name = "JavaSE-17",
+            path = home .. "/.sdkman/candidates/java/17.0.6-tem",
+          },
+          {
+            name = "JavaSE-11",
+            path = home .. "/.sdkman/candidates/java/11.0.18-tem",
+          },
+          {
+            name = "JavaSE-1.8",
+            path = home .. "/.sdkman/candidates/java/8.0.332-librca",
+          },
         },
       },
+      maven = {
+        downloadSources = true,
+      },
+      implementationsCodeLens = {
+        enabled = true,
+      },
+      referenceCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      format = {
+        enabled = true,
+        settings = {
+          url = home .. "/.config/nvim/java/checkstyle.xml",
+          profile = "ModpmStyle",
+        },
+      },
+
       signatureHelp = { enabled = true },
+
       contentProvider = { preferred = "fernflower" }, -- Use fernflower to decompile library code
+
       -- Specify any completion options
       completion = {
         favoriteStaticMembers = {
@@ -121,66 +176,15 @@ local config = {
         },
         useBlocks = true,
       },
-      -- If you are developing in projects with different Java versions, you need
-      -- to tell eclipse.jdt.ls to use the location of the JDK for your Java version
-      -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-      -- And search for `interface RuntimeOption`
-      -- The `name` is NOT arbitrary, but must match one of the elements from `enum ExecutionEnvironment` in the link above
-      configuration = {
-        runtimes = {
-          {
-            name = "JavaSE-17",
-            path = home .. "/.sdkman/candidates/java/17.0.6-tem",
-          },
-          {
-            name = "JavaSE-11",
-            path = home .. "/.sdkman/candidates/java/11.0.18-tem",
-          },
-          {
-            name = "JavaSE-1.8",
-            path = home .. "/.sdkman/candidates/java/8.0.332-librca",
-          },
-        },
+
+      flags = {
+        allow_incremental_sync = true,
+      },
+
+      init_options = {
+        bundles = {},
       },
     },
-  },
-  -- cmd is the command that starts the language server. Whatever is placed
-  -- here is what is passed to the command line to execute jdtls.
-  -- Note that eclipse.jdt.ls must be started with a Java version of 17 or higher
-  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
-  -- for the full list of options
-  cmd = {
-    home .. "/.sdkman/candidates/java/17.0.6-tem/bin/java",
-    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-    "-Dosgi.bundles.defaultStartLevel=4",
-    "-Declipse.product=org.eclipse.jdt.ls.core.product",
-    "-Dlog.protocol=true",
-    "-Dlog.level=ALL",
-    "-Xmx4g",
-    "--add-modules=ALL-SYSTEM",
-    "--add-opens",
-    "java.base/java.util=ALL-UNNAMED",
-    "--add-opens",
-    "java.base/java.lang=ALL-UNNAMED",
-    -- If you use lombok, download the lombok jar and place it in ~/.local/share/eclipse
-    "-Xbootclasspath/p:"
-      .. home
-      .. "/.config/nvim/java/lombok.jar",
-    "-javaagent:" .. home .. "/.config/nvim/java/lombok.jar",
-
-    -- The jar file is located where jdtls was installed. This will need to be updated
-    -- to the location where you installed jdtls
-    "-jar",
-    home .. "/dev/env/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
-
-    -- The configuration for jdtls is also placed where jdtls was installed. This will
-    -- need to be updated depending on your environment
-    "-configuration",
-    home .. "/dev/env/jdtls/plugins/config_mac",
-
-    -- Use the workspace_folder defined above to store data for this project
-    "-data",
-    workspace_folder,
   },
 }
 
